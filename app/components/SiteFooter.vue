32f5507e3374165b8361c241af8ae037058a73f8
yunxw <151393032+yunxw@users.noreply.github.com>
2025-07-20 20:07:16 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/yunxw/site-status",
+  home: "https://yunx.fun",
+  email: "https://yunx.fun",
 };
 </script>
 
